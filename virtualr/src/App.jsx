66e061ed3d3dee103fae417a85e8002178f16630
zoll-h1 <nurbekerkibaev9@gmail.com>
2026-03-17 15:01:34 +0600

    Feat : HeroSection

--- virtualr/src/App.jsx
+++ virtualr/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import NavBar from './components/NavBar'
+import React from 'react';
+import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
 const App = () => {
   return (
     <div>
       <NavBar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/>
+      </div>
     </div>
   )
 }
